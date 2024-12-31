0386782eef80fb6d9616640f6a87c5602cc0cfdc
Joshua Farayola <farayolajoshua@gmail.com>
2024-12-31 16:32:25 +0100

    implement project update and member management

--- src/project/project.resolver.ts
+++ src/project/project.resolver.ts
@@ -1,5 +1,7 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GraphQLError } from 'graphql';
 import { CreateProjectArgs } from './dtos/create-project.args';
+import { UpdateProjectArgs } from './dtos/update-project.args';
 import { Project } from './models/project.model';
 import { ProjectService } from './project.service';
 
@@ -37,5 +39,34 @@ export class ProjectResolver {
       name,
       memberIds,
     );
+  }
+
+  @Mutation(() => Boolean)
+  async updateProject(@Args() { id, sessionId, name }: UpdateProjectArgs) {
+    if (!id && !sessionId) {
+      throw new GraphQLError('Either `id` or `sessionId` must be provided.', {
+        extensions: {
+          code: 'BAD_USER_INPUT',
+        },
+      });
+    }
+
+    return this.projectService.updateProject(id || sessionId, { name });
+  }
+
+  @Mutation(() => Boolean)
+  async addProjectMember(
+    @Args('projectId', { type: () => Int }) projectId: number,
+    @Args('userId', { type: () => Int }) userId: number,
+  ) {
+    return this.projectService.addMember(projectId, userId);
+  }
+
+  @Mutation(() => Boolean)
+  async removeProjectMember(
+    @Args('projectId', { type: () => Int }) projectId: number,
+    @Args('userId', { type: () => Int }) userId: number,
+  ) {
+    return this.projectService.removeMember(projectId, userId);
   }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -114,4 +114,140 @@ export class ProjectService {
       throw error;
     }
   }
+
+  /**
+   * Update a project using it id or session id.
+   * @param idOrSessionId Project id or the session id of the project
+   * @param data Project fields to update
+   * @returns true if update occurred, false otherwise.
+   */
+  async updateProject(
+    idOrSessionId: number | string,
+    {
+      name,
+    }: {
+      name?: string;
+    },
+  ) {
+    const project = await this.prisma.project.updateMany({
+      where: {
+        OR: [
+          {
+            id:
+              typeof idOrSessionId === 'string'
+                ? parseInt(idOrSessionId)
+                : idOrSessionId,
+          },
+          {
+            sessionId:
+              typeof idOrSessionId === 'number'
+                ? idOrSessionId.toString()
+                : idOrSessionId,
+          },
+        ] as const,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return project.count == 1;
+  }
+
+  /**
+   *
+   * @param idOrSessionId A project id or its associated session id
+   * @param userId The user id to add to the project
+   * @returns true, throws an error if the project or user cannot be found.
+   */
+  async addMember(idOrSessionId: number | string, userId: number) {
+    let projectId =
+      typeof idOrSessionId === 'string'
+        ? (
+            await this.prisma.project.findUnique({
+              where: { sessionId: idOrSessionId },
+            })
+          ).id
+        : idOrSessionId;
+
+    try {
+      await this.prisma.projectMembership.upsert({
+        where: {
+          userId_projectId: {
+            userId,
+            projectId,
+          },
+        },
+        update: {},
+        create: {
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+          project: {
+            connect: {
+              id: projectId,
+            },
+          },
+        },
+      });
+
+      return true;
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new GraphQLError('Project or user cannot be found.', {
+          extensions: { code: 'NOT_FOUND' },
+        });
+      }
+      throw error;
+    }
+  }
+
+  /**
+   * @param idOrSessionId A project id or its associated session id
+   * @param userId The user id to remove from the project
+   * @returns true if the user was removed, false otherwise.
+   */
+  async removeMember(idOrSessionId: number | string, userId: number) {
+    const project = await this.prisma.project.findFirst({
+      where: {
+        OR: [
+          {
+            id:
+              typeof idOrSessionId === 'string'
+                ? parseInt(idOrSessionId)
+                : idOrSessionId,
+          },
+          {
+            sessionId:
+              typeof idOrSessionId === 'number'
+                ? idOrSessionId.toString()
+                : idOrSessionId,
+          },
+        ],
+      },
+    });
+
+    if (!project) return false;
+
+    if (userId === project.createdById) {
+      throw new GraphQLError('Cannot remove the project creator.', {
+        extensions: { code: 'BAD_REQUEST' },
+      });
+    }
+
+    let projectId = project.id;
+
+    const membership = await this.prisma.projectMembership.delete({
+      where: {
+        userId_projectId: {
+          userId,
+          projectId,
+        },
+      },
+    });
+
+    return membership !== null;
+  }
 }
