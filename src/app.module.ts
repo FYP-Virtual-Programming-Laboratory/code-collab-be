a143f613ea4a5a670c5a25ce85c20bf64ee733e2
Joshua Farayola <farayolajoshua@gmail.com>
2025-02-25 00:49:18 +0100

    refactor GraphQL module to use async configuration and add user authentication context

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,42 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLError } from 'graphql';
 import { join } from 'path';
 import { AppService } from './app.service';
 import { FileModule } from './file/file.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectModule } from './project/project.module';
 import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      imports: [UserModule],
+      inject: [UserService],
+      useFactory: async (userService: UserService) => ({
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        context: async ({ req }) => {
+          const userId = parseInt(req.headers.userId);
+
+          if (userId && (await userService.findUserById(userId))) {
+            return { userId };
+          }
+
+          throw new GraphQLError('User does not exist', {
+            extensions: {
+              code: 'UNAUTHENTICATED',
+              http: {
+                statusCode: 401,
+              },
+            },
+          });
+        },
+      }),
     }),
     FileModule,
     PrismaModule,
